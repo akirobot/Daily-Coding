3531070893d4a34829829478128e2d4bee2b3874
Kevin Kobata <akirobot@users.noreply.github.com>
2024-02-07 13:54:41 -0800

    Create p548clockangle.java

---
+++ code/p548clockangle.java
@@ -0,0 +1,10 @@
+/*
+ * Given a clock time in hh:mm format, determine, to the nearest degree, the angle betwen the hour and minute hands.
+ * 
+ * Assuming the minute hand ticks between each minute and doesn't move constantly.
+ * 
+ * Following clockwise rotation, so if the hour was at 12, and the minutes were 50, it would be the entirety from 12 to 10. The counter clockwise angle could be calculated to get that angle too.
+ */
+public class p548clockangle {
+    
+}
